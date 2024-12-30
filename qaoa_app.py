1b3b6bf6fcceecc1158cc50a0b855a5aa987c0ae
Leonardo Lavagna <llavagna@proton.me>
2024-12-30 17:14:15 +0100

    Update qaoa_app.py

--- qaoa_app.py
+++ qaoa_app.py
@@ -40,7 +40,7 @@ def solve_maxcut(p, problem, mixer, verbose):
     betas = qaoa_utils.generate_parameters(n=p, k=1)
     gammas = qaoa_utils.generate_parameters(n=p, k=2)
     qaoa = Q.Qaoa(p=p, G=problem, betas=betas, gammas=gammas, mixer=mixer, verbose=verbose)
-    x, f = optims.simple_optimization(qaoa, seed=seed, verbose=verbose)
+    x, f = optims.simple_optimization(qaoa, verbose=verbose)
     st.write("MaxCut Solution:")
     st.write(f"MaxCut Value: -f")
 
