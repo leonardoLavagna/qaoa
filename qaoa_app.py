34d58c2ff04caff0a6f700bc5cc6ff6744b409e0
Leonardo Lavagna <llavagna@proton.me>
2024-12-30 17:29:50 +0100

    Update qaoa_app.py

--- qaoa_app.py
+++ qaoa_app.py
@@ -26,7 +26,7 @@ def create_instance():
     problem = P.Problems(G=G)
     return p, problem, mixer
 
-def plot(p,problem, G, mixer):
+def plot(p,problem, mixer):
     st.header("Problem instance and associated QAOA circuit")
     plt.figure(figsize=(6,6))
     nx.draw(G, with_labels=True, font_weight='bold', node_color='lightblue', edge_color='gray')
