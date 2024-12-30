7a2593030d540120e78ecd2d77871afd593b67ba
Leonardo Lavagna <llavagna@proton.me>
2024-12-30 17:28:52 +0100

    Update qaoa_app.py

--- qaoa_app.py
+++ qaoa_app.py
@@ -26,10 +26,10 @@ def create_instance():
     problem = P.Problems(G=G)
     return p, problem, mixer
 
-def plot(p,problem,mixer):
+def plot(p,problem, G, mixer):
     st.header("Problem instance and associated QAOA circuit")
     plt.figure(figsize=(6,6))
-    nx.draw(problem.G, with_labels=True, font_weight='bold', node_color='lightblue', edge_color='gray')
+    nx.draw(G, with_labels=True, font_weight='bold', node_color='lightblue', edge_color='gray')
     qaoa = Q.Qaoa(p=p, G=problem, mixer=mixer)
     st.pyplot(plt)
     st.write(qaoa.get_circuit())
@@ -48,7 +48,7 @@ def main():
     st.title("MaxCut Problem Solver with QAOA")
     p, problem, mixer = create_instance()
     if len(problem.G.edges) > 0:
-        plot(p,problem,mixer)
+        plot(p,problem, G, mixer)
         if st.button("Solve MaxCut"):
             solve_maxcut(p,problem,mixer)
 
