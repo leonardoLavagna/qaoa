1237f662cfdf5e77e23951f9866aa1ef0fe53da9
Leonardo Lavagna <llavagna@proton.me>
2024-12-30 17:27:54 +0100

    Update qaoa_app.py

--- qaoa_app.py
+++ qaoa_app.py
@@ -32,7 +32,7 @@ def plot(p,problem,mixer):
     nx.draw(problem.G, with_labels=True, font_weight='bold', node_color='lightblue', edge_color='gray')
     qaoa = Q.Qaoa(p=p, G=problem, mixer=mixer)
     st.pyplot(plt)
-    st.write(print(qaoa.get_circuit()))
+    st.write(qaoa.get_circuit())
 
 def solve_maxcut(p, problem, mixer):
     st.header("Solving MaxCut with QAOA...")
