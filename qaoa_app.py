5bb2a4912e811aaa40ba15cb46e4eb51cf2987b3
Leonardo Lavagna <llavagna@proton.me>
2025-01-03 12:40:27 +0100

    Update qaoa_app.py

--- qaoa_app.py
+++ qaoa_app.py
@@ -55,7 +55,7 @@ def plot_graph_partition(problem, p, mixer, x):
     job = backend.run(t_qc)
     counts = job.result().get_counts(qc)
     most_frequent_solution = max(counts, key=counts.get)
-    partition_1, partition_2 = get_partitions_from_solution(G, most_frequent_solution)
+    partition_1, partition_2 = get_partitions_from_solution(qaoa.G, most_frequent_solution)
     plt.figure(figsize=(6, 6))
     node_colors = ['lightblue' if node in partition_1 else 'orange' for node in qaoa.G.nodes()]
     nx.draw(
