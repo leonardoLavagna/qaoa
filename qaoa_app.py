bb80ca6a811948174880f0972b64ea4dd17a6fe7
Leonardo Lavagna <llavagna@proton.me>
2025-01-03 12:05:58 +0100

    Update qaoa_app.py

--- qaoa_app.py
+++ qaoa_app.py
@@ -47,7 +47,6 @@ def main():
     st.title("MaxCut Problem Solver with QAOA")
     p, problem, G, mixer = create_instance()
     if len(problem.G.edges) > 0:
-        plot_graph(G)
         plot_circuit_and_graph(p,problem, mixer)
         if st.button("Solve MaxCut"):
             solve_maxcut(p,problem,mixer)
