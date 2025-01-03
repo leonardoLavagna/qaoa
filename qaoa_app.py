de995cefc9e56286d989c03cafc8f435cd43e895
Leonardo Lavagna <llavagna@proton.me>
2025-01-03 12:31:58 +0100

    Update qaoa_app.py

--- qaoa_app.py
+++ qaoa_app.py
@@ -46,7 +46,8 @@ def plot_graph_partition(problem, p, mixer, x):
     G = qaoa.G
     qc = qaoa.get_circuit()
     qc = qc.assign_parameters(init_point)
-    t_qc = transpile(qc, backend=Aer.get_backend("aer_simulator"))
+    backend = Aer.get_backend("aer_simulator")
+    t_qc = transpile(qc, backend=backend)
     job = backend.run(t_qc, shots=shots)
     counts = job.result().get_counts(qc)
     st.write(counts)
